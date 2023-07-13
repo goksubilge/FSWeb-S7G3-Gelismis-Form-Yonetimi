32c65143cefb7d36c4498798eae8c815d36d16fe
Bilge <134232792+goksubilge@users.noreply.github.com>
2023-07-13 19:53:41 +0300

    add errors and disabled

--- kullanici-katilimi/src/components/Form.js
+++ kullanici-katilimi/src/components/Form.js
@@ -37,6 +37,11 @@ const Form = () => {
   const [formData, setFormData] = useState(ilkFormData);
   // şimdi yazıkça güncellenmesi için chanceHandler ekliyorum. inputlarda oluyor tabiki.
 
+  //bir tane hataları tutan state, bir tane d eform hatası tutan 2 state tanımlıyorum.
+
+  const [errors, setErrors] = useState({});
+  const [isDisable, setisDisable] = useState(true);
+
   const changeHandler = (e) => {
     //console.log("change handler kontrolü", e.target.name);
     // console.log ("event", e);
@@ -57,10 +62,18 @@ const Form = () => {
     Yup.reach(userKurallar, e.target.name)
       .validate(value)
       .then((valid) => {
+        setisDisable(false);
         console.log("valid", valid);
       })
       .catch((err) => {
-        console.log("error", err);
+        setisDisable(true);
+        console.log("error verdi", err);
+
+        const writeTotalErrs = {
+          ...errors,
+          [e.target.name]: err.errors[0],
+        };
+        setErrors(writeTotalErrs);
       });
 
     setFormData(newFormData);
@@ -76,6 +89,7 @@ const Form = () => {
           name="name"
           value={formData.name}
         />
+        {errors.name && <p>{errors.name}</p>}
       </div>
       <div>
         <label htmlFor="email">E-Mail</label>
@@ -85,6 +99,7 @@ const Form = () => {
           name="email"
           value={formData.email}
         />
+        {errors.email && <p>{errors.email}</p>}
       </div>
       <div>
         <label htmlFor="pass">Key Pls</label>
@@ -94,6 +109,7 @@ const Form = () => {
           name="pass"
           value={formData.pass}
         />
+        {errors.pass && <p>{errors.pass}</p>}
       </div>
       <div>
         <label htmlFor="terms">Kullanım Şartları</label>
@@ -103,9 +119,16 @@ const Form = () => {
           name="terms"
           checked={formData.terms}
         />
+        {errors.terms && <p>{errors.terms}</p>}
       </div>
-      <button type="submit">SEND</button>
+      <button type="submit" disabled={!isDisable}>
+        SEND
+      </button>
     </form>
   );
 };
 export default Form;
+
+// DISABLE değilse then valid setdisable(değil) ; disable (true ise) catch setdisable(true) hatayı yakala ve error yaz uyarı ver.
+// BU KONUDA DİSABLE KISMI VE ERROR EKLEME EN ZOR KISIM OLDU!
+// ERRORLARI DA YİNE CATCH DE TANIMLAYIP YAKALAYIP YAZDIRIYORUM. [e.target.name]: err.errors(0) hangi hata varsa objede bir array olarak tutuyor  error(0) sayesinde.
